06eef15b6764960f6c5b4d304ee6935616d69856
anik-98 <anikjana98@gmail.com>
2022-06-23 13:07:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-color-mode-eghkkj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-color-mode-eghkkj)
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Title>Demo of @nuxtjs/color-mode</Title>
+    <ColorModePicker />
+    <p>
+      Go to
+      <NuxtLink to="/light"> light page </NuxtLink>
+    </p>
+  </div>
+</template>
